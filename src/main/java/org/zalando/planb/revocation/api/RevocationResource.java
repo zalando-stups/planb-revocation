b1bd87bd72681ad600ada12cfa6b35223248d5f6
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2016-03-14 19:19:38 +0100

    #50 Updated POST /revocations docs (Problem responses still missing)

--- src/main/java/org/zalando/planb/revocation/api/RevocationResource.java
+++ src/main/java/org/zalando/planb/revocation/api/RevocationResource.java
@@ -8,7 +8,7 @@ import org.zalando.planb.revocation.domain.RevocationInfo;
 /**
  * Resource to get and post revocations.
  *
- * @author <a href="mailto:rodrigo.reis@zalando.de">Rodrigo Reis</a>
+ * @author  <a href="mailto:rodrigo.reis@zalando.de">Rodrigo Reis</a>
  */
 public interface RevocationResource {
 
@@ -17,16 +17,20 @@ public interface RevocationResource {
      *
      * <p>Also returns meta information which may be of importance to the client, like a refresh notification.</p>
      *
-     * @param from instant from when tokens were revoked, in UTC UNIX timestamp
-     * @return all the revocations since the specified timestamp
+     * @param   from  instant from when tokens were revoked, in UTC UNIX timestamp
+     *
+     * @return  all the revocations since the specified timestamp
      */
     RevocationInfo get(int from);
 
     /**
      * Posts the specified revocation to be stored.
      *
-     * @param revocation instant from when they were revoked, in UTC UNIX timestamp
-     * @return HTTP Status {@code CREATED}, if the revocation was successfully stored
+     * <p>Revokes tokens associated with the specified revocation type.</p>
+     *
+     * @param   revocation  the revocation associated with the tokens to revoke
+     *
+     * @return  HTTP Status {@code CREATED}, if the revocation was successfully stored
      */
     HttpEntity<String> post(Revocation revocation);
 }
