a09ca2e0c26254796b6a86b88f1874083c7a1191
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-04-01 16:13:55 +0200

    more tests for UnixTimeStamp

--- src/main/java/org/zalando/planb/revocation/util/UnixTimestamp.java
+++ src/main/java/org/zalando/planb/revocation/util/UnixTimestamp.java
@@ -1,14 +1,23 @@
 package org.zalando.planb.revocation.util;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
 import java.util.Date;
 
-public class UnixTimestamp {
+public abstract class UnixTimestamp {
+
+    private UnixTimestamp() {
+    }
 
     public static int now() {
-        return (int) (System.currentTimeMillis() / 1000);
+        return internalToSeconds(System.currentTimeMillis());
     }
 
     public static int of(Date date) {
-        return (int) (date.getTime() / 1000);
+        return internalToSeconds(date.getTime());
+    }
+
+    protected static int internalToSeconds(long millis) {
+        return (int) MILLISECONDS.toSeconds(millis);
     }
 }
