2cb4dbbc2fb93e26db8f0afc6391042fd7d462db
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2016-04-04 18:50:56 +0200

    #7ß Added issued_before to token revocation data

--- src/main/java/org/zalando/planb/revocation/domain/RevokedTokenInfo.java
+++ src/main/java/org/zalando/planb/revocation/domain/RevokedTokenInfo.java
@@ -3,11 +3,13 @@ package org.zalando.planb.revocation.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+import org.zalando.planb.revocation.util.UnixTimestamp;
 
 /**
  * <ul>
  *     <li>{@code tokenHash}: The revoked token hashed using hash_algorithm, in URL Base64 encoding;</li>
  *     <li>{@code hashAlgorithm}: The algorithm used for hashing the Token.</li>
+ *     <li>{@code issuedBefore}: a UNIX Timestamp (UTC) indicating that tokens issued before it are revoked.</li>
  * </ul>
  *
  * @author  <a href="mailto:rodrigo.reis@zalando.de">Team Greendale</a>
@@ -19,4 +21,7 @@ public class RevokedTokenInfo implements RevokedInfo {
 
     @JsonProperty("hash_algorithm")
     private String hashAlgorithm;
+
+    @JsonProperty("issued_before")
+    private Integer issuedBefore;
 }
