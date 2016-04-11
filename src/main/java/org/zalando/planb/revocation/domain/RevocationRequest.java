bf0eaf8819fd92e23cd30229b83fc904ee8d1d8a
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2016-04-11 13:09:37 +0200

    #59 Started development of test utils for domain objects

--- src/main/java/org/zalando/planb/revocation/domain/RevocationRequest.java
+++ src/main/java/org/zalando/planb/revocation/domain/RevocationRequest.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 /**
- * Holds a new revocation
+ * Holds a new revocation request.
  *
  * @author <a href="mailto:rodrigo.reis@zalando.de">Rodrigo Reis</a>
  */
@@ -16,8 +16,18 @@ import org.immutables.value.Value;
 @JsonDeserialize(as = ImmutableRevocationRequest.class)
 public interface RevocationRequest {
 
+    /**
+     * Returns the type of this revocation request.
+     *
+     * @return the type of this revocation request
+     */
     RevocationType type();
 
+    /**
+     * Returns the details of this revocation request.
+     *
+     * @return the details of this revocation request.
+     */
     @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "type")
     @JsonSubTypes(
             {
