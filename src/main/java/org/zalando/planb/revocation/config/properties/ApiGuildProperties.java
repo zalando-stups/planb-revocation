ee1b6cb3582fe59360c7fde3adbcc8f4dd055bfa
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2016-04-13 11:40:58 +0200

    #97 Removed Lombok from ApiGuildProperties

--- src/main/java/org/zalando/planb/revocation/config/properties/ApiGuildProperties.java
+++ src/main/java/org/zalando/planb/revocation/config/properties/ApiGuildProperties.java
@@ -1,17 +1,23 @@
 package org.zalando.planb.revocation.config.properties;
 
+import com.google.common.base.Preconditions;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import lombok.Data;
-
 /**
- * TODO: small javadoc
+ * Contains properties used for configuring the application according to Zalando's API Guild.
  *
- * @author  <a href="mailto:team-greendale@zalando.de">Team Greendale</a>
+ * @author  <a href="mailto:rodrigo.reis@zalando.de">Rodrigo Reis</a>
  */
-@Data
 @ConfigurationProperties(prefix = "api")
 public class ApiGuildProperties {
 
     private String swaggerFile = "classpath:api/swagger.yml";
+
+    public String getSwaggerFile() {
+        return swaggerFile;
+    }
+
+    public void setSwaggerFile(String swaggerFile) {
+        this.swaggerFile = Preconditions.checkNotNull(swaggerFile);
+    }
 }
