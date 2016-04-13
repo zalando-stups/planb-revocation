9ed502f3f8680472e63c2563762c2715e7f221af
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2016-04-13 12:05:36 +0200

    #97 Added tests for ApiGuildProperties

--- src/main/java/org/zalando/planb/revocation/config/properties/ApiGuildProperties.java
+++ src/main/java/org/zalando/planb/revocation/config/properties/ApiGuildProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * Contains properties used for configuring the application according to Zalando's API Guild.
  *
- * @author  <a href="mailto:rodrigo.reis@zalando.de">Rodrigo Reis</a>
+ * @author <a href="mailto:rodrigo.reis@zalando.de">Rodrigo Reis</a>
  */
 @ConfigurationProperties(prefix = "api")
 public class ApiGuildProperties {
@@ -17,7 +17,7 @@ public class ApiGuildProperties {
         return swaggerFile;
     }
 
-    public void setSwaggerFile(String swaggerFile) {
+    public void setSwaggerFile(final String swaggerFile) {
         this.swaggerFile = Preconditions.checkNotNull(swaggerFile);
     }
 }
---
+++ src/test/java/org/zalando/planb/revocation/config/properties/ApiGuildPropertiesTest.java
@@ -0,0 +1,26 @@
+package org.zalando.planb.revocation.config.properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zalando.planb.revocation.config.PlanBRevocationConfig;
+
+/**
+ * Unit tests for {@link ApiGuildProperties}.
+ *
+ * @author <a href="mailto:rodrigo.reis@zalando.de">Rodrigo Reis</a>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = PlanBRevocationConfig.class)
+public class ApiGuildPropertiesTest {
+
+    @Autowired
+    private ApiGuildProperties properties;
+
+    @Test(expected = NullPointerException.class)
+    public void testNullPointerExceptionWhenNullValue() {
+        properties.setSwaggerFile(null);
+    }
+}
