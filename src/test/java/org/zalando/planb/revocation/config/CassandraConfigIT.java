3366c8c9d5379e92d24fa04c3db14f2215def5d5
Andre Hartmann <andre.hartmann@zalando.de>
2016-04-15 17:31:58 +0200

    #52 tested cassandra connection with bad / missing credentials

---
+++ src/test/java/org/zalando/planb/revocation/config/CassandraConfigIT.java
@@ -0,0 +1,57 @@
+package org.zalando.planb.revocation.config;
+
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.AuthenticationException;
+import org.junit.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+import static org.springframework.boot.Banner.Mode.OFF;
+
+public class CassandraConfigIT {
+
+    @Test
+    public void testSuccessfulCassandraConnection() throws Exception {
+        final ConfigurableApplicationContext applicationContext =
+                new SpringApplicationBuilder(CassandraConfig.class)
+                        .web(false)
+                        .bannerMode(OFF)
+                        .profiles("it")
+                        .run();
+
+        assertThat(applicationContext.getBean(Session.class)).isNotNull();
+
+        applicationContext.close();
+    }
+
+    @Test
+    public void testCassandraConnectionWithBadCredentials() throws Exception {
+        try {
+            new SpringApplicationBuilder(CassandraConfig.class)
+                    .web(false)
+                    .bannerMode(OFF)
+                    .profiles("it")
+                    .run("--cassandra.password=theWrongOne");
+            failBecauseExceptionWasNotThrown(BeansException.class);
+        } catch (BeansException e) {
+            assertThat(e).hasRootCauseInstanceOf(AuthenticationException.class);
+        }
+    }
+
+    @Test
+    public void testCassandraConnectionWithMissingCredentials() throws Exception {
+        try {
+            new SpringApplicationBuilder(CassandraConfig.class)
+                    .web(false)
+                    .bannerMode(OFF)
+                    .run("--cassandra.contact-points=127.0.0.1");
+            failBecauseExceptionWasNotThrown(BeansException.class);
+        } catch (BeansException e) {
+            assertThat(e).hasRootCauseInstanceOf(AuthenticationException.class);
+        }
+    }
+
+}
